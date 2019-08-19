3556bf7377bc8258085f547364fa8367cfc688d1
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-19 15:11:22 -0700

    corrected emjoi use

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 // function implementation
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log(`🔴🔴🔴 Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`✅✅✅ Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+
   }
 
 };
