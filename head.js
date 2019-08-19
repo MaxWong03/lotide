1b46ef264193bd5ba7c584e6af0174234b8f555f
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-19 15:13:03 -0700

    head returns the first element of an array

---
+++ head.js
@@ -0,0 +1,29 @@
+//assertEqual 
+
+// function implementation
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+
+
+//end of assertEqual 
+
+//head
+const head = (arr) => {
+  return arr[0];
+};
+
+//end of head
+
+
+
+
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
