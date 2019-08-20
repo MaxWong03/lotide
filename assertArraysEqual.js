9f66cb38d17bfc9f9461dcd532c1453155272524
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-20 12:51:03 -0700

    this function print out an appropriate message to the console after evualting whether or not two array are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,47 @@
+//check if an array is empty
+const isEmptyArr = (arr) => {
+  let isEmpty = false;
+  arr.length === 0 ? isEmpty = true : isEmpty = false;
+  return isEmpty;
+};
+
+const isSameLengthArr = (arr1, arr2) => {
+  let isSameLength = false;
+  const [arr1Length, arr2Length] = [arr1.length, arr2.length];
+  arr1Length === arr2Length ? isSameLength = true : isSameLength = false;
+  return isSameLength;
+};
+
+
+
+const eqArrays = (arr1, arr2) => {
+  //if no arrgument or only 1 arrugemnt, return false
+  if (arr1 === undefined || arr2 === undefined) {
+    return false;
+  } else if (isEmptyArr(arr1) || isEmptyArr(arr2)) { //if one of the array is empty, return false
+    return false;
+  } else if (!isSameLengthArr(arr1, arr2)) { //if 2 arrays are not of the same length, return false
+    return false;
+  } else { //check if two arr is equal
+    for (let [index, val] of arr1.entries()) {
+      if (val !== arr2[index]) {
+        return false;
+      }
+    }
+
+  }
+
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  let message = '';
+  eqArrays(arr1,arr2) === true ? message = `✅✅✅ Assertion Passed: ${arr1} === ${arr2}` : message = `🔴🔴🔴 Assertion Failed: ${arr1} !== ${arr2}`;
+  console.log(message);
+};
+
+//testcase
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
