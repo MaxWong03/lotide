14e210d9611228a9ac619ae666be26cb46385a85
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-26 14:20:09 -0700

    export all functions with @hhaslam11

---
+++ index.js
@@ -0,0 +1,39 @@
+const assertArraysEqual = require('./src/assertArraysEqual');
+const assertEqual = require('./src/assertEqual');
+const assertObjectsEqual = require('./src/assertObjectsEqual');
+const countLetters = require('./src/countLetters');
+const countOnly = require('./src/countOnly');
+const eqArrays = require('./src/eqArrays');
+const eqObjects = require('./src/eqObjects');
+const findKey = require('./src/findKey');
+const findKeyByValue = require('./src/findKeyByValue');
+const flatten = require('./src/flatten');
+const head = require('./src/head');
+const letterPositions = require('./src/letterPositions');
+const map = require('./src/map');
+const middle = require('./src/middle');
+const tail = require('./src/tail');
+const takeUntil = require('./src/takeUntil');
+const without = require('./src/without');
+
+
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  head: head,
+  letterPositions: letterPositions,
+  map: map,
+  middle: middle,
+  tail: tail,
+  takeUntil: takeUntil,
+  without: without
+};
