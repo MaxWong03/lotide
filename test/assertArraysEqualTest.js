bde54fe5ef93e750f8c29f842b8173e07eca1e39
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-26 10:20:12 -0700

    refactor assertArrayEqual, includes test code for function

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,7 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+//testcase
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
