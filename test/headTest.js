92f435e220a0da74371c868c4b85a64d7b186b3d
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-26 11:56:19 -0700

    refactor using mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,21 @@
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
 
-
-
-
-assertEqual(head([]), undefined);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('#head', () => {
+  it('should return undefined if input array is empty', () => {
+    const arr = [];
+    assert.strictEqual(head(arr), undefined);
+  });
+  it('should return "Hello" if input array is ["Hello", "Lighthouse", "Labs"]', () => {
+    const arr = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(head(arr), 'Hello');
+  });
+  it('should return 1 if input array is [1,2,3]', () => {
+    const arr = [1, 2, 3];
+    assert.strictEqual(head(arr), 1);
+  });
+  it('should return 5 if input array is [5]', () => {
+    const arr = [5];
+    assert.strictEqual(head(arr), 5);
+  });
+});
