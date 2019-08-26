a63922d49523e47fe7eb8719deac831722de4d99
MaxWong03 <madmaxmmajournal@gmail.com>
2019-08-26 20:08:22 +0000

    refactor middle using mocha chai with @hhaslam11

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,29 @@
 const middle = require('../src/middle');
-const assertArraysEqual = require('../src/assertArraysEqual');
+const assert = require('chai').assert;
+
+//test case
+describe('middle', () => {
+  it('should return [2] when input array is [1,2,3]', () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+  it('it should return [2,3] when input array is [1,2,3,4]', () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([]), []);
+  });
+  it('it should return empty array when input array length is <=2 ', () => {
+    assert.deepEqual(middle([1]), []);
+  });
+});
 
 
-//test cases
-assertArraysEqual(middle([1,2,3]), [2]);
-assertArraysEqual(middle([1,2,3,4]), [2,3]);
-assertArraysEqual(middle([1,2,3,4,5]), [3]);
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-assertArraysEqual(middle([1,2]), []);
+/*
+it('', () => {
+
+});
+*/
+
